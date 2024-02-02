d97b4d5186d80bbf9f79586a2a6cf4f4c3401c65
Hân <hanphan1509@gmail.com>
2024-02-02 18:57:44 +0700

    linked list

--- Module2/Program.cs
+++ Module2/Program.cs
@@ -9,97 +9,125 @@ namespace Module2
 {
     internal class Program
     {
+        public static Student[] myStudents = new Student[4];
         static void Main(string[] args)
         {
-            //1.
-            Student myStudent1 = new Student("Nguyễn Văn A", 10, "5D4");
-            Student myStudent2 = new Student("Nguyễn Văn B", 9, "4C3");
-            Student myStudent3 = new Student("Nguyễn Văn C", 8, "3B2");
-            Student myStudent4 = new Student("Nguyễn Văn D", 7, "2A1");
-            //2.
-            Student[] myStudents = new Student[4];
+            Student myStudent1 = new Student(0, "Nguyễn Văn A", 10, "5D4");
+            Student myStudent2 = new Student(1, "Nguyễn Văn B", 9, "4C3");
+            Student myStudent3 = new Student(2, "Nguyễn Văn C", 8, "3B2");
+            Student myStudent4 = new Student(3, "Nguyễn Văn D", 7, "2A1");
             myStudents[0] = myStudent1;
             myStudents[1] = myStudent2;
             myStudents[2] = myStudent3;
             myStudents[3] = myStudent4;
-            //3.
-            ShowAllStudents(myStudents);
-            //4.
-            SearchName(myStudents, "Nguyễn Văn A");
+
+            //1. Create Student LinkedList
+            LinkedList<Student> studentList = new LinkedList<Student>();
+            //2. Add student in LinkedList
+            studentList.AddLast(myStudent1);
+            studentList.AddLast(myStudent2);
+            studentList.AddLast(myStudent3);
+            studentList.AddLast(myStudent4);
+            //3. Show first student
+            Console.WriteLine("\nFirst student:");
+            ShowInfo(studentList.First.Value);
+            //4. Show last student
+            Console.WriteLine("\nLast student:");
+            ShowInfo(studentList.Last.Value);
             //5.
-            SearchAge(myStudents, true);
+            Console.WriteLine("\nShow all student:");
+            ShowAllStudentInLinkedList(studentList);
             //6.
-            SearchAge(myStudents, false);
-            //7.
-            ChangeStudent(myStudents, 0, 3);
-        }
-
-        private static void ChangeStudent(Student[] students, int oldPos, int newPos)
-        {
-            Student tempPos;
-            tempPos = students[oldPos];
-            students[oldPos] = students[newPos];
-            students[newPos] = tempPos;
-            Console.WriteLine("\nChange position students: ");
-            ShowAllStudents(students);
-        }
-
-        private static void SearchAge(Student[] students, bool isHighest)
-        {
-            int indexStudent = 0;
-            int tempAge = students[0].Age;
-            for(int i = 0; i < students.Length; i++)
+            Student searchNameStudent = SearchNameLinkedList(studentList, "Nguyễn Văn A");
+            if (searchNameStudent != null)
             {
-                if(isHighest)
+                Console.WriteLine("\nSearch student name:");
+                ShowInfo(searchNameStudent);
+            }
+            else
+                Console.WriteLine("No result");
+            //7.
+            Student searchhighestAge = SearchAgeLinkedList(studentList, true);
+            ShowInfo(searchhighestAge);
+            //8.
+            Student searchlowestAge = SearchAgeLinkedList(studentList, false);
+            ShowInfo(searchlowestAge);
+            //9.
+            ChangePositionStudentLinkedList(studentList, 3);
+        }
+
+        private static void ChangePositionStudentLinkedList(LinkedList<Student> studentList, int newPos)
+        {
+            Student tempStudent = studentList.First.Value;
+            Student valueNewPos = studentList.First.Value;
+            LinkedListNode<Student> p = studentList.First;
+            for (int i = 0; i < newPos; i++)
+            {
+                p = p.Next;
+                if (i == newPos - 1)
                 {
-                    if (tempAge < students[i].Age)
-                    {
-                        tempAge = students[i].Age;
-                        indexStudent = i;
-                    }
-                } else
+                    valueNewPos = p.Value;
+                    p.Value = tempStudent;
+                }
+            }
+            studentList.First.Value = valueNewPos;
+
+            Console.WriteLine("\nChange position students: ");
+            ShowAllStudentInLinkedList(studentList);
+        }
+
+        private static void ShowInfo(Student student)
+        {
+            Console.WriteLine($"Position: {student.Positon} - Name: {student.Name} - Age: {student.Age} - Class: {student.Grade}");
+        }
+
+        private static Student SearchAgeLinkedList(LinkedList<Student> studentList, bool isHighest)
+        {
+            LinkedListNode<Student> indexNode = studentList.First;
+            int tempAge = studentList.First.Value.Age;
+            for (LinkedListNode<Student> p = studentList.First; p != null; p = p.Next)
+            {
+                if (isHighest)
                 {
-                    if (tempAge > students[i].Age)
+                    if (tempAge < p.Value.Age)
                     {
-                        tempAge = students[i].Age;
-                        indexStudent = i;
+                        tempAge = p.Value.Age;
+                        indexNode = p;
                     }
                 }
-                
+                else
+                {
+                    if (tempAge > p.Value.Age)
+                    {
+                        tempAge = p.Value.Age;
+                        indexNode = p;
+                    }
+                }
             }
-            Console.WriteLine($"\nSearch {(isHighest? "highest" : "lowest")} age student:");
-            ShowStudentInfomation(students[indexStudent]);
+
+            Console.WriteLine($"\nSearch {(isHighest ? "highest" : "lowest")} age student:");
+            return indexNode.Value;
         }
 
-        private static void SearchName(Student[] students, string nameSearch)
+        private static Student SearchNameLinkedList(LinkedList<Student> studentList, string nameSearch)
         {
-            Student searchStudent = null;
-            foreach(Student student in students)
+            Student temp = null;
+            foreach (Student student in studentList)
             {
                 if (student.Name == nameSearch)
                 {
-                    searchStudent = student;
-                }    
+                    temp = student;
+                }
             }
-
-            Console.WriteLine("\nSearch name student:");
-            if (searchStudent != null)
-                ShowStudentInfomation(searchStudent);
-            else
-                Console.WriteLine("No result");
+            return temp;
         }
 
-        private static void ShowAllStudents(Student[] students)
+        private static void ShowAllStudentInLinkedList(LinkedList<Student> studentList)
         {
-            foreach (Student student in students)
+            foreach (Student student in studentList)
             {
-                ShowStudentInfomation(student);
+                ShowInfo(student);
             }
-        }
-
-        private static void ShowStudentInfomation(Student myStudent)
-        {
-            Console.WriteLine($"My Student: {myStudent.Name} - {myStudent.Age} - {myStudent.Grade}");
         }
     }
 }
--- Module2/Student.cs
+++ Module2/Student.cs
@@ -8,19 +8,22 @@ namespace Module2
 {
     internal class Student
     {
+        private int positon;
         private string name;
         private int age;
         private string grade;
 
-        public Student(string name, int age, string grade)
+        public Student(int positon, string name, int age, string grade)
         {
-            this.Name = name;
-            this.Age = age;
-            this.Grade = grade;
+            this.positon = positon;
+            this.name = name;
+            this.age = age;
+            this.grade = grade;
         }
 
         public string Name { get => name; set => name = value; }
         public int Age { get => age; set => age = value; }
         public string Grade { get => grade; set => grade = value; }
+        public int Positon { get => positon; set => positon = value; }
     }
 }
